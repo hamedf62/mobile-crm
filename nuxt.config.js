9c05a56a3e1f3faaf88a92bca463278bdc9950a9
Hamed Fallah <hamedf@gmail.com>
2022-06-11 20:35:36 +0430

    correcting nuxt

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,8 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  ssr: false, // Disable Server Side rendering
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - mobile_crm',
